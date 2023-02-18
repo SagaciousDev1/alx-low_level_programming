e86db46ff2b4da1f7e901d96fe636e4054ce43dd
SagaciousDev1 <mordiobinna@gmail.com>
2023-02-17 20:12:49 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.>
+
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc = 'a';
+
+	while (lc <= 'z')
+	{
+		putchar(lc);
+
+		lc++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
